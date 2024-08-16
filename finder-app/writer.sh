87b74fbaa33ce1e10d0efe0adc6437f3d2fbeb6a
Parth M <parth11dec@gmail.com>
2024-08-16 00:01:24 -0700

    This commmit adds files for Assignment 1
    * Modified files to verify unity framework
    * Creates writer and finder scripts

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+WRITEDIR=/tmp/aeld-data
+
 # Check for required arguments
 if [ $# -ne 2 ]; then
   echo "Error: Two arguments required: writefile and writestr"
@@ -9,8 +11,10 @@ fi
 writefile="$1"
 writestr="$2"
 
+mkdir -p "$WRITEDIR"
+
 # Create the file and write the content
-if ! echo "$writestr" > "$writefile"; then
+if ! echo "$WRITEDIR/$writestr" > "$writefile"; then
   echo "Error: Could not create file '$writefile'"
   exit 1
 fi
